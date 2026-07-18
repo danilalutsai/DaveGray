d50d55e43389fa1932c756408ac7899c8caee9f9
Danila Lutsai <dlutsai98@gmail.com>
2026-07-18 10:28:12 +0300

    Debounce

---
+++ 13.Debounce/main01.js
@@ -0,0 +1,29 @@
+const initApp = () => {
+  const button = document.querySelector("#button")
+  // button.addEventListener("click", debounce(clickLog, 2000))
+  button.addEventListener('click', () => {
+    clickLog()
+    button.disabled = true
+    setTimeout(() => {
+      button.disabled = false
+    }, 2000)
+  })
+}
+
+const clickLog = () => console.log('Clicked')
+
+document.addEventListener('DOMContentLoaded', initApp)
+
+const debounce = (fn, delay) => {
+  let id
+  console.log(`id at immediate load: ${id}`)
+
+  return (...args) => {
+    console.log(`previous id: ${id}`)
+
+    if (id) clearTimeout(id)
+    id = setTimeout(() => {
+      fn(...args)
+    }, delay)
+  }
+}
